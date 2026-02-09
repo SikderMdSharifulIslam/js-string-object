f94cd85ae0ef965bfe296df386ff6b6c2fb440f5
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-09 19:21:20 +0600

    object keys, values, nested object and delete propety

---
+++ keys.js
@@ -0,0 +1,15 @@
+const computer = {
+    brand: 'apple',
+    price: 2000,
+    processor: 'm5',
+    hdd: '512gb',
+    monitor: '14.2inch'
+}
+
+// keys
+const keys = Object.keys(computer);
+console.log(keys);
+
+// values
+const values = Object.values(computer);
+console.log(values);
---
+++ nested.js
@@ -0,0 +1,19 @@
+const college = {
+    name: 'vnc',
+    class: ['11', '12'],
+    events: ['science fair', 'bijoy dibos', '21 feb'],
+    unique: {
+        color: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+}
+
+console.log(college.unique.result.gpa);
+
+// delete property from object
+delete college.class;
+
+console.log(college);
