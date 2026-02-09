59af862c61a70535e8da892bfbe08e3aa70d1621
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-09 17:22:01 +0600

    more object related info

--- object.js
+++ object.js
@@ -7,11 +7,17 @@ const bottle = {
     bottleName: {
         bottle_a: 'A',
         bottle_b: 'B'
+    },
+    taste: function(){
+        console.log("Good");
     }
 }
 
+bottle.taste();
+
 // object non-primitive
-// object er moddhe key-value pair kore info rakha hoy and ekhane array, even object o rakha jay.
+// object er moddhe key-value pair kore info rakha hoy and ekhane array, function (method bole), even object o rakha jay.
+// object mainly akta powerful variable jar moddhe amra onk gulo information k key-value pair kore rakhte pari
 
 // primitive mane single, direct value store kore amn kisu
 // and non-primitive hosse complex
