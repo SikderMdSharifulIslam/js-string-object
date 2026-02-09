12f0d4d9f2420cc39d30fe98e42281fa3bb8d85b
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-09 17:07:50 +0600

    js object added

---
+++ object.js
@@ -0,0 +1,17 @@
+const bottle = {
+    brnad: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false,
+    bottleSize: [1, 2, 3],
+    bottleName: {
+        bottle_a: 'A',
+        bottle_b: 'B'
+    }
+}
+
+// object non-primitive
+// object er moddhe key-value pair kore info rakha hoy and ekhane array, even object o rakha jay.
+
+// primitive mane single, direct value store kore amn kisu
+// and non-primitive hosse complex
